25718b741d4b4c12b5b4a66c51221a083bfb36b3
Alaric Holloway <alaric.holloway@gmail.com>
2015-04-23 22:17:44 -0700

    Rename: note -> task

--- sql/create.sql
+++ sql/create.sql
@@ -1,14 +1,20 @@
 create table tudu_user (
     user_id serial primary key,
     email varchar(128) not null,
-    pw_hash varchar(256) not null,
+    password_hash varchar(256) not null,
     date_created timestamp not null default current_timestamp
 );
 
-create table tudu_note (
-    note_id serial primary key,
-    note_text text not null,
-    -- ordinal double precision not null unique,
+create table tudu_task (
+    task_id serial primary key,
+    task_description text not null,
+    /* index this. tasks will be displayed in reverse-chronological order */
+    date_created timestamp not null default current_timestamp,
+    /* null date indicates task has not been completed */
+    /* index this. partition on this also? there will not be much interaction
+     * with completed tasks. in fact, all interaction with completed tasks will
+     * take place in the history viewer. partitioning could make sense. */
+    date_completed timestamp,
     user_id integer references tudu_user
 );
 
@@ -17,18 +23,26 @@ create table tudu_tag (
     tag_text varchar(32) unique
 );
 
-create table tudu_note_tag (
-    -- if a note is deleted, delete corresponding note/tag link
-    note_id integer references tudu_note on delete cascade,
-    -- forbid deletion of tags that are linked to notes
+create table tudu_task_tag (
+    -- if a task is deleted, delete corresponding task/tag link
+    task_id integer references tudu_task on delete cascade,
+    -- forbid deletion of tags that are linked to tasks
     tag_id integer references tudu_tag on delete restrict,
-    unique (note_id, tag_id)
+    unique (task_id, tag_id)
+);
+
+create table tudu_access_token (
+    token_id serial primary key,
+    token_string char(256) not null unique,
+    date_created timestamp not null default current_timestamp,
+    revoked boolean not null default false,
+    user_id integer references tudu_user
 );
 
 -- Triggers --------------------------------------------------------------------
 
 /**
- * Whenever a tag is removed from a note, delete the tag if no other notes
+ * Whenever a tag is removed from a task, delete the tag if no other tasks
  * reference it.
  */
 create or replace function attempt_tag_deletion() returns trigger as $$
@@ -41,15 +55,7 @@ exception
 end;
 $$ language plpgsql security definer;
 
-create trigger tudu_note_tag_deletion
-after delete on tudu_note_tag
+create trigger tudu_task_tag_deletion
+after delete on tudu_task_tag
 for each row
 execute procedure attempt_tag_deletion();
-
-/*create table tudu_access_token (
-    token_id serial primary key,
-    token_string char(256) not null unique,
-    date_created timestamp not null default current_timestamp,
-    revoked boolean not null default false,
-    user_id integer references tudu_user
-);*/
--- sql/test.sql
+++ sql/test.sql
@@ -7,23 +7,23 @@ create or replace function play() returns void as $$
 declare
     _uid integer;
 begin
-    insert into tudu_user (email, pw_hash) values('foo@bar.com', 'xxx');
+    insert into tudu_user (email, password_hash) values('foo@bar.com', 'xxx');
     
-    insert into tudu_note (note_text, user_id) values('Red Wine Gastrique', 1);
-    insert into tudu_note (note_text, user_id) values('Bordeaux', 1);
+    insert into tudu_task (task_description, user_id) values('Red Wine Gastrique', 1);
+    insert into tudu_task (task_description, user_id) values('Bordeaux', 1);
     
     insert into tudu_tag (tag_text) values('food');
     insert into tudu_tag (tag_text) values('wine');
     
-    insert into tudu_note_tag (note_id, tag_id) values (1, 1);
-    insert into tudu_note_tag (note_id, tag_id) values (1, 2);
-    insert into tudu_note_tag (note_id, tag_id) values (2, 2);
+    insert into tudu_task_tag (task_id, tag_id) values (1, 1);
+    insert into tudu_task_tag (task_id, tag_id) values (1, 2);
+    insert into tudu_task_tag (task_id, tag_id) values (2, 2);
     
     /* fails, as it should */
     /* delete from tudu_tag where tag_id = 1; */
     
-    /* should succeed and cascade delete to tudu_note_tag */
-    delete from tudu_note where note_id = 1;
+    /* should succeed and cascade delete to tudu_task_tag */
+    delete from tudu_task where task_id = 1;
     
     /* 'food' tag should have been deleted automatically */
 end;
@@ -32,4 +32,4 @@ $$ language plpgsql security definer;
 select play();
 
 select * from tudu_tag;
-select * from tudu_note_tag;
+select * from tudu_task_tag;
