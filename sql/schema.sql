6b1e376ba797a93db35a365bcfacd7d8f952f857
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-05 13:52:42 -0700

    Description should never be null

--- sql/schema.sql
+++ sql/schema.sql
@@ -46,7 +46,7 @@ create index tudu_user_log_kvs_idx on tudu_user_log using gin (kvs);
 create table tudu_task (
     task_id                 bigint primary key default nextval('tudu_task_seq'),
     user_id                 bigint references tudu_user,
-    description             varchar(256),
+    description             varchar(256) not null,
     tags                    varchar(64)[] default null,
     finished_date           timestamptz,
     --
