1671413f7e6d31d8b11fe47b651f89b166123d47
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-04 20:22:17 -0700

    Add more utility functions

--- sql/lib/util.sql
+++ sql/lib/util.sql
@@ -1,6 +1,24 @@
 /**
  * Trim whitespace (including newlines) from input string
  */
-create or replace function util.btrim_whitespace(varchar) returns varchar as $$
-    select btrim($1, E'\n\r\t ');
+create or replace function util.btrim_whitespace(_str text) returns text as $$
+    select btrim(_str, E'\n\r\t ');
+$$ language sql immutable security definer;
+
+/**
+ * Trim whitespace (including newlines) from strings in array
+ */
+create or replace function util.btrim_whitespace(_str_arr text[]) returns text[] as $$
+    select array_agg(x.val) from (select util.btrim_whitespace(unnest(_str_arr)) as val) x;
+$$ language sql immutable security definer;
+
+/**
+ * Remove duplicates, nulls, and empty strings from an array, sorting it
+ * ascending and trimming whitespace in the process. If only PostgreSQL had
+ * first-class functions...
+ */
+create or replace function util.sort_dedup_denull_btrim_whitespace(_str_array text[]) returns text[] as $$
+    select array_agg(x.val) from (
+        select distinct util.btrim_whitespace(unnest(_str_array)) as val order by val asc
+    ) x where x.val not in (null, '');
 $$ language sql immutable security definer;
--- sql/test/util.sql
+++ sql/test/util.sql
@@ -13,3 +13,31 @@ begin
     return _message;
 end;
 $$ language plpgsql;
+
+create or replace function unit_tests.btrim_whitespace_array() returns test_result as $$
+declare
+    _message    test_result;
+begin
+    if util.btrim_whitespace(array[E'\n   foo \t \r', '  bar  ']) <> array['foo', 'bar'] then
+        select assert.fail('should trim all trailing and leading whitespace for all entries of input array') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.sort_dedup_denull_btrim_whitespace() returns test_result as $$
+declare
+    _message    test_result;
+begin
+    if util.sort_dedup_denull_btrim_whitespace(array[E'3 \n', E'\n 3', '1', '  2  ', '1', '2', E'\r 2 \t']) <> array['1', '2', '3'] then
+        select assert.fail('should remove duplicate elements, sorting them ascending in the process') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
