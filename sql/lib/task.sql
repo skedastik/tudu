74bb139bee010e0e23b67c8d347c6bb68d2afab6
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-05 15:35:23 -0700

    Implement delete_task() and finish_task()

--- sql/lib/task.sql
+++ sql/lib/task.sql
@@ -74,32 +74,87 @@ end;
 $$ language plpgsql security definer;
 
 /**
- * Mark an existing task as finished.
+ * Mark an existing task as deleted.
  * 
  * Arguments
+ *   _task_id   ID of existing task
+ *   _ip        Optional IP address
  * 
  * Returns
  *   ID of task on success
+ *   -1 if task ID is invalid
+ *   -2 if task is already deleted
  */
-create or replace function tudu.finish_task(
+create or replace function tudu.delete_task(
+    _task_id    bigint,
+    _ip         inet        default null
 ) returns bigint as $$
+declare
+    _status     varchar;
 begin
-    /* TODO */
+    select task_id, status into _task_id, _status from tudu_task where task_id = _task_id;
+    
+    if _task_id is null then
+        return -1;
+    end if;
+    
+    if _status = 'deleted' then
+        return -2;
+    end if;
+    
+    update tudu_task
+    set status = 'deleted',
+        edate  = now()
+    where task_id = _task_id;
+    
+    perform tudu.task_log_add(_task_id, 'delete', _ip);
+    
+    return _task_id;
 end;
 $$ language plpgsql security definer;
 
 /**
- * Mark an existing task as deleted.
+ * Mark an existing task as finished.
  * 
  * Arguments
+ *   _task_id   ID of existing task
+ *   _ip        Optional IP address
  * 
  * Returns
  *   ID of task on success
+ *   -1 if task ID is invalid
+ *   -2 if task is already finished
+ *   -3 if task is in an incompatible state (e.g. deleted)
  */
-create or replace function tudu.delete_task(
+create or replace function tudu.finish_task(
+    _task_id    bigint,
+    _ip         inet        default null
 ) returns bigint as $$
+declare
+    _status     varchar;
 begin
-    /* TODO */
+    select task_id, status into _task_id, _status from tudu_task where task_id = _task_id;
+    
+    if _task_id is null then
+        return -1;
+    end if;
+    
+    if _status = 'finished' then
+        return -2;
+    end if;
+    
+    if _status <> 'init' then
+        return -3;
+    end if;
+    
+    update tudu_task
+    set status = 'finished',
+        edate  = now()
+    where task_id = _task_id;
+    
+    perform tudu.task_log_add(_task_id, 'finish', _ip);
+    
+    return _task_id;
 end;
 $$ language plpgsql security definer;
 
--- sql/test/task.sql
+++ sql/test/task.sql
@@ -93,7 +93,7 @@ begin
         return _message;
     end if;
     
-    if _task_log is null or _task_log.task_id <> _task_id then
+    if _task_log.task_id <> _task_id then
         select assert.fail('should create a task log entry with matching task_id') into _message;
         return _message;
     end if;
@@ -128,6 +128,211 @@ begin
     
     if _task_log.operation = 'set_tags' then
         select assert.fail('should NOT create a task log entry') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.delete_task() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+    _task_log   tudu_task_log%ROWTYPE;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    _task_id  := tudu.delete_task(_task.task_id);
+    _task     := tudu.latest_task();
+    _task_log := tudu.latest_task_log();
+    
+    if _task_id <> _task.task_id then
+        select assert.fail('should succeed') into _message;
+        return _message;
+    end if;
+    
+    if _task.status <> 'deleted' then
+        select assert.fail('should set status to "deleted"') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.task_id <> _task_id then
+        select assert.fail('should create a task log entry with matching task_id') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.operation <> 'delete' then
+        select assert.fail('should create a task log entry with operation "delete"') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.delete_task_using_invalid_task_id() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+    _task_log   tudu_task_log%ROWTYPE;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    _task_id  := tudu.delete_task(-1);
+    _task     := tudu.latest_task();
+    _task_log := tudu.latest_task_log();
+    
+    if _task_id <> -1 then
+        select assert.fail('should fail') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.operation = 'delete' then
+        select assert.fail('should NOT create a task log entry') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.delete_task_that_has_already_been_deleted() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    perform tudu.delete_task(_task.task_id);
+    _task_id  := tudu.delete_task(_task.task_id);
+    _task     := tudu.latest_task();
+    
+    if _task_id <> -2 then
+        select assert.fail('should fail') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.finish_task() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+    _task_log   tudu_task_log%ROWTYPE;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    _task_id  := tudu.finish_task(_task.task_id);
+    _task     := tudu.latest_task();
+    _task_log := tudu.latest_task_log();
+    
+    if _task_id <> _task.task_id then
+        select assert.fail('should succeed') into _message;
+        return _message;
+    end if;
+    
+    if _task.status <> 'finished' then
+        select assert.fail('should set status to "finished"') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.task_id <> _task_id then
+        select assert.fail('should create a task log entry with matching task_id') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.operation <> 'finish' then
+        select assert.fail('should create a task log entry with operation "finish"') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.finish_task_using_invalid_task_id() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+    _task_log   tudu_task_log%ROWTYPE;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    _task_id  := tudu.finish_task(-1);
+    _task     := tudu.latest_task();
+    _task_log := tudu.latest_task_log();
+    
+    if _task_id <> -1 then
+        select assert.fail('should fail') into _message;
+        return _message;
+    end if;
+    
+    if _task_log.operation = 'finish' then
+        select assert.fail('should NOT create a task log entry') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.finish_task_that_has_already_been_finished() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    perform tudu.finish_task(_task.task_id);
+    _task_id  := tudu.finish_task(_task.task_id);
+    _task     := tudu.latest_task();
+    
+    if _task_id <> -2 then
+        select assert.fail('should fail') into _message;
+        return _message;
+    end if;
+    
+    select assert.ok('End of test.') into _message;
+    return _message;
+end;
+$$ language plpgsql;
+
+create or replace function unit_tests.finish_task_in_incompatible_state() returns test_result as $$
+declare
+    _message    test_result;
+    _user       tudu_user%ROWTYPE;
+    _task       tudu_task%ROWTYPE;
+    _task_id    bigint;
+begin
+    _user     := tudu.create_random_user();
+    _task     := tudu.create_random_task(_user.user_id);
+    perform tudu.delete_task(_task.task_id);
+    _task_id  := tudu.finish_task(_task.task_id);
+    _task     := tudu.latest_task();
+    
+    if _task_id <> -3 then
+        select assert.fail('should fail for a task that has been deleted') into _message;
         return _message;
     end if;
     
