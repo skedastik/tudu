8f2a2b8b56d68c8ca3fbd531a376cf4c5abcae2b
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-09 23:37:17 -0700

    Introduce email validator

---
+++ server/core/data/validate/Email.php
@@ -0,0 +1,34 @@
+<?php
+namespace Tudu\Core\Data\Validate;
+
+require_once __DIR__.'/Validate.php';
+
+/**
+ * Shorthand constructor.
+ * 
+ * @return Tudu\Core\Data\Validate\Email An email validator.
+ */
+function Email() {
+    return new Email();
+}
+
+/**
+ * String validator.
+ */
+class Email extends Validate {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->noun = "Email address";
+    }
+    
+    protected function _validate($data) {
+        // email validation is intentionally lax
+        if (preg_match('/^[^@]+@[^@]+\.[^@]+$/', $data) !== 1) {
+            return "is invalid.";
+        }
+        
+        return $this->pass($data);
+    }
+}
+?>
--- server/test/core/data/ValidateTest.php
+++ server/test/core/data/ValidateTest.php
@@ -2,16 +2,59 @@
 namespace Tudu\Test\Core\Data\Validate;
 
 require_once __DIR__.'/../../../core/data/validate/String.php';
+require_once __DIR__.'/../../../core/data/validate/Email.php';
 require_once __DIR__.'/../../../../vendor/autoload.php';
 
 use \Tudu\Core\Data\Validate;
 
 class StringTest extends \PHPUnit_Framework_TestCase {
 
-    public function testLengthValidation() {
+    public function testLengthLowerBound() {
+        $validator = Validate\String()->length()->from(10);
+        $this->assertNull($validator->validate('this string is valid even if it is rather long'));
+        $this->assertNotNull($validator->validate('too short'));
+    }
+
+    public function testLengthUpperBound() {
+        $validator = Validate\String()->length()->upto(5);
+        $this->assertNull($validator->validate('valid'));
+        $this->assertNotNull($validator->validate('toooooooooooooooooo long'));
+    }
+
+    public function testLengthRange() {
         $validator = Validate\String()->length()->from(10)->upto(15);
         $this->assertNull($validator->validate('valid string'));
-        $this->assertNotNull($validator->validate('invalid'));
+        $this->assertNotNull($validator->validate('too short'));
+        $this->assertNotNull($validator->validate('toooooooooooooooooo long'));
+    }
+}
+
+class EmailTest extends \PHPUnit_Framework_TestCase {
+
+    public function testEmailFormat() {
+        $validator = Validate\Email();
+        $this->assertNull($validator->validate('valid@email.xyz'));
+        $this->assertNotNull($validator->validate('@invalid@email@xyz'));
+    }
+}
+
+class ChainingTest extends \PHPUnit_Framework_TestCase {
+
+    public function testChainTwoValidators() {
+        $validator = Validate\Email()->also(Validate\String()->length()->upto(15));
+        $this->assertNull($validator->validate('valid@email.xyz'));
+        $this->assertNotNull($validator->validate('@invalid@email@xyz'));
+        $this->assertNotNull($validator->validate('this_email_is@too_long.xyz'));
+    }
+    
+    public function testChainThreeValidators() {
+        $validator = Validate\Email()
+            ->also(Validate\String()->length()->from(15))
+            ->also(Validate\String()->length()->upto(20));
+        $this->assertNull($validator->validate('just_right@valid.xyz'));
+        $this->assertNotNull($validator->validate('@invalid@email@xyz'));
+        $this->assertNotNull($validator->validate('this_email_is@too_long.xyz'));
+        $this->assertNotNull($validator->validate('too@short.xyz'));
     }
 }
   
