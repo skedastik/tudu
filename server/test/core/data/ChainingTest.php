1a0bda75321469408141e4301c7cea4f6e7586aa
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-11 21:30:10 -0700

    Test chaining both ways

--- server/test/core/data/ChainingTest.php
+++ server/test/core/data/ChainingTest.php
@@ -6,10 +6,16 @@ use \Tudu\Core\Data\Validate\Validate;
 
 class ChainingTest extends \PHPUnit_Framework_TestCase {
     
-    public function testTransformValidateChain() {
+    public function testTransformThenValidate() {
         $chain = Transform::ToString()->interpret()->boolean()
             ->then(Validate::String()->length()->upTo(1));
         $this->assertNull($chain->execute('truthy'));
+    }
+    
+    public function testValidateThenTransform() {
+        $chain = Validate::String()->length()->upTo(1)
+            ->then(Transform::ToString()->interpret()->boolean());
+        $this->assertNotNull($chain->execute('truthy'));
     }
 }
   
