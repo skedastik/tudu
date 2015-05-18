0178673d25897455a97a41bef412dce6eba5d527
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-18 15:10:45 -0700

    ModelMutateTest -> ModelTest
    Also, add tests for array conversion and getters/setters.

--- server/test/core/data/model/ModelTest.php
+++ server/test/core/data/model/ModelTest.php
@@ -5,7 +5,33 @@ use \Tudu\Core\Data\Transform\Transform;
 use \Tudu\Core\Data\Validate\Validate;
 use \Tudu\Test\Fixture\FakeModel;
 
-class ModelMutateTest extends \PHPUnit_Framework_TestCase {
+class ModelTest extends \PHPUnit_Framework_TestCase {
+    
+    public function testArrayConversion() {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'sooperdooper@abc.xyz'
+        ];
+        $model = new FakeModel($data);
+        $this->assertSame($data, $model->asArray());
+    }
+    
+    public function testGetSet() {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'sooperdooper@abc.xyz'
+        ];
+        $model = new FakeModel($data);
+        $this->assertSame($data['name'], $model->get('name'));
+        
+        $newName = 'Jane Doe';
+        $model->set('name', $newName);
+        $expected = [
+            'name' => $newName,
+            'email' => 'sooperdooper@abc.xyz'
+        ];
+        $this->assertSame($expected, $model->asArray());
+    }
     
     public function testMutation() {
         $model = new FakeModel([
