17b03fe23743e0215c33c8de29fe6ed501a425da
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-19 17:37:47 -0700

    Grant “tudu” schema usage to basic role

--- sql/role.sql
+++ sql/role.sql
@@ -1,12 +1,15 @@
 revoke all on tudu_user from tudu_role_web;
 revoke all on tudu_task from tudu_role_web;
 revoke all on all functions in schema tudu from tudu_role_web;
+revoke all on schema tudu from tudu_role_web;
 
 drop role tudu_role_web;
 create role tudu_role_web;
 
 grant select on table tudu_user to tudu_role_web;
 grant select on table tudu_task to tudu_role_web;
+grant usage on schema tudu to tudu_role_web;
 grant execute on all functions in schema tudu to tudu_role_web;
 
+/* drop role tudu_user_web; */
 /* create role tudu_user_web with login encrypted password 'changeme' in role tudu_role_web; */
