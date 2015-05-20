f46dce98543af0ac4a71c3b394545554dec527b9
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-19 20:41:11 -0700

    Accept optional custom error descriptions

--- server/core/data/repository/Error.php
+++ server/core/data/repository/Error.php
@@ -27,35 +27,50 @@ class Error {
      * @param string $description (optional)
      * @param array $context Key/value array describing the resource
      */
-    public static function Generic($description = null, $context = null) {
+    public static function Generic(
+        $description = null,
+        $context = null
+    ) {
         return new Core\Error(Error::GENERIC, $description, $context);
     }
     
     /**
      * Shorthand factory function for validation errors.
      * 
+     * @param string $description (optional)
      * @param array $context Key/value array describing the resource
      */
-    public static function Validation($context = null) {
-        return new Core\Error(Error::VALIDATION, 'Invalid resource descriptor.', $context);
+    public static function Validation(
+        $description = 'Invalid resource descriptor.',
+        $context = null
+    ) {
+        return new Core\Error(Error::VALIDATION, $description, $context);
     }
     
     /**
      * Shorthand factory function for "already in use" errors.
      * 
+     * @param string $description (optional)
      * @param array $context Key/value array describing the resource
      */
-    public static function AlreadyInUse($context = null) {
+    public static function AlreadyInUse(
+        $description = null,
+        $context = null
+    ) {
         return new Core\Error(Error::ALREADY_IN_USE, null, $context);
     }
     
     /**
      * Shorthand factory function for "resource not found" errors.
      * 
+     * @param string $description (optional)
      * @param array $context Key/value array describing the resource
      */
-    public static function ResourceNotFound($context = null) {
-        return new Core\Error(Error::RESOURCE_NOT_FOUND, 'The specified resource could not be found.', $context);
+    public static function ResourceNotFound(
+        $description = 'The specified resource could not be found.',
+        $context = null
+    ) {
+        return new Core\Error(Error::RESOURCE_NOT_FOUND, $description, $context);
     }
 }
 ?>
