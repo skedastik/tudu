b7e28193a6c9c4bc933c447beb44d014b4d27c2f
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-21 19:46:23 -0700

    Add TODO

--- server/handler/api/Users.php
+++ server/handler/api/Users.php
@@ -1,6 +1,11 @@
 <?php
 namespace Tudu\Handler\Api;
 
+use \Tudu\Data\Repository;
+use \Tudu\Data\Model;
+use \Tudu\Core;
+use \Tudu\Core\Data\Repository\Error;
+
 /**
  * Request handler for /users/
  */
@@ -10,8 +15,48 @@ final class Users extends \Tudu\Core\Handler\API {
         return 'POST';
     }
     
+    /**
+     * POST to "/users/" to sign up a new user.
+     */
     protected function post() {
-        echo 'Users->post()';
+        $userRepo = new Repository\User($this->db);
+        $data = json_decode($this->delegate->getRequestBody(), true);
+        
+        /**
+         * TODO: Consider: The password must be sent in plain-text. This means
+         * we need to add a 'password' validator to the Model, which in turn
+         * means we need to normalize the model prior to passing its data to the
+         * repo. But the repo already normalizes data--so a double normalize
+         * will take place.
+         * 
+         * Solution: Do not call normalize from the repo. Instead, always
+         * normalize prior to passing it data.
+         * 
+         * The question then becomes: How do you enforce the requirement that
+         * all data passed to a repo must be normalized?
+         */
+        
+        $result = $userRepo->signupUser(
+            $data['email'],
+            $data['password_hash'],
+            $this->delegate->getRequestIp()
+        );
+        
+        if ($result instanceof Core\Error) {
+            $statusCode = $result->getHttpStatusCode();
+            $this->delegate->setResponseStatus(is_null($statusCode) ? 400 : $statusCode);
+            $responseBody = json_encode($result->asArray());
+        } else {
+            /**
+             * TODO: Provide link to new resource.
+             */
+            $this->delegate->setResponseStatus(201);
+            $this->delegate->setResponseHeaders([
+                'Location' => '/users/'.$result->get('user_id')
+            ]);
+        }
+        
+        echo $responseBody;
     }
 }
 
