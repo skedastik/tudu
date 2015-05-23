2afcb89becd7614bf9f48b43ee5591b39253a59e
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-22 21:52:03 -0700

    SelectTest.php -> QueryTest.php

--- server/test/integration/database/QueryTest.php
+++ server/test/integration/database/QueryTest.php
@@ -3,13 +3,19 @@ namespace Tudu\Test\Integration\Database;
 
 use \Tudu\Test\Integration\Database\DatabaseTest;
 
-class SelectTest extends DatabaseTest {
+class QueryTest extends DatabaseTest {
     
-    public function testSelect() {
+    public function testQuerySelect() {
         $result = $this->db->query('select 123 as val;');
         $this->assertNotFalse($result);
         $this->assertEquals(1, count($result));
         $this->assertEquals(123, $result[0]['val']);
+    }
+    
+    public function testQueryValueSelect() {
+        $result = $this->db->queryValue('select 123;');
+        $this->assertNotFalse($result);
+        $this->assertEquals(123, $result);
     }
 }
 ?>
