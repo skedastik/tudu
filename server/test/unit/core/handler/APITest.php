d6ffc351a6a19e7320e0ae66db2f64c1d3f13869
Alaric Holloway <alaric.holloway@gmail.com>
2015-05-25 17:46:35 -0700

    Expand test coverage

--- server/test/unit/core/handler/APITest.php
+++ server/test/unit/core/handler/APITest.php
@@ -31,6 +31,18 @@ class APITest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(201, $this->app->getResponseStatus());
     }
     
+    public function testPostWithWildcardAcceptHeaderReturns201() {
+        $this->app->setRequestMethod('POST');
+        $this->app->setRequestHeader('Accept', '*/*');
+        $this->app->setRequestHeader('Content-Type', 'application/json');
+        $this->app->setRequestBody('{
+            "name": "John Doe",
+            "email": "johndoe@foo.xyz"
+        }');
+        $this->app->run();
+        $this->assertEquals(201, $this->app->getResponseStatus());
+    }
+    
     public function testValidPostReturnsApplicationJsonContentType() {
         $this->app->setRequestMethod('POST');
         $this->app->setRequestHeader('Content-Type', 'application/json');
@@ -43,8 +55,40 @@ class APITest extends \PHPUnit_Framework_TestCase {
         $this->assertNotFalse($mediaType->compare(new MediaType('application/json')));
     }
     
+    public function testOptionsRequestReturns200() {
+        $this->app->setRequestMethod('OPTIONS');
+        $this->app->run();
+        $this->assertEquals(200, $this->app->getResponseStatus());
+    }
+    
+    public function testOptionsRequestReturnsAllowHeader() {
+        $this->app->setRequestMethod('OPTIONS');
+        $this->app->run();
+        $this->assertEquals($this->handler->getAllowedMethods(), $this->app->getResponseHeader('Allow'));
+    }
+    
     public function testUnsupportedRequestMethodReturns405() {
+        $this->app->setRequestMethod('GET');
+        $this->app->run();
+        $this->assertEquals(405, $this->app->getResponseStatus());
+        
+        $this->app->setResponseStatus(null);
         $this->app->setRequestMethod('PUT');
+        $this->app->run();
+        $this->assertEquals(405, $this->app->getResponseStatus());
+        
+        $this->app->setResponseStatus(null);
+        $this->app->setRequestMethod('PATCH');
+        $this->app->run();
+        $this->assertEquals(405, $this->app->getResponseStatus());
+        
+        $this->app->setResponseStatus(null);
+        $this->app->setRequestMethod('DELETE');
+        $this->app->run();
+        $this->assertEquals(405, $this->app->getResponseStatus());
+        
+        $this->app->setResponseStatus(null);
+        $this->app->setRequestMethod('HEAD');
         $this->app->run();
         $this->assertEquals(405, $this->app->getResponseStatus());
     }
