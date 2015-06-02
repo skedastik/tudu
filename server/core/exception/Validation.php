8b5f745ce1c962013e8891ce7de43dadb01ab7fa
Alaric Holloway <alaric.holloway@gmail.com>
2015-06-01 18:23:59 -0700

    Recommend context data for validation errors

--- server/core/exception/Validation.php
+++ server/core/exception/Validation.php
@@ -4,7 +4,8 @@ namespace Tudu\Core\Exception;
 /**
  * Validation exception class.
  * 
- * Validation exceptions indicate data validation errors.
+ * Validation exceptions indicate data validation errors. Such errors should
+ * always carry context data indicating which fields failed to validate and why.
  */
 class Validation extends Client {
     
