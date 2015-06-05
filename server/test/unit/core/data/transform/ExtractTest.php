80d1cd5b0a5ff7b1c1d2f7597e04ee3a773e71d7
Alaric Holloway <alaric.holloway@gmail.com>
2015-06-04 17:05:29 -0700

    Test multiline input string

--- server/test/unit/core/data/transform/ExtractTest.php
+++ server/test/unit/core/data/transform/ExtractTest.php
@@ -3,9 +3,6 @@ namespace Tudu\Test\Unit\Core\Data\Transform;
 
 use \Tudu\Core\Data\Transform\Transform;
 
-/**
- * @group todo
- */
 class ExtractTest extends \PHPUnit_Framework_TestCase {
     
     private $transformer;
@@ -36,6 +33,17 @@ class ExtractTest extends \PHPUnit_Framework_TestCase {
         $expected = [
             'ﾀﾁﾂﾃ',
             'food&سلام'
+        ];
+        $this->assertSame($expected, $this->transformer->execute($input));
+    }
+    
+    public function testExtractHashtagsShouldWorkWithMultilineInput() {
+        $input = "Buy # noodles...#phoSure.\n#food&wine #foo#bar\n";
+        $expected = [
+            'phoSure',
+            'food&wine',
+            'foo',
+            'bar'
         ];
         $this->assertSame($expected, $this->transformer->execute($input));
     }
